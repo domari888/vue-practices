cae5effecb5fb1387c704f3b2173d191b90dd913
damari <motokin128@gmail.com>
2022-12-29 13:54:55 +0900

    add: リストレンダリングで表示している配列に新しい要素を追加するボタンとメソッドを追加
    データバインディングしている key 属性を一意にするために要素を追加する際に id の最大値を取得してそれに 1 をプラスする形にして重複しないように要素を追加

--- v_for/index.js
+++ v_for/index.js
@@ -6,5 +6,21 @@ const vFor = new Vue({
       { name: 'B氏', id: 2 },
       { name: 'C氏', id: 3 }
     ]
+  },
+  methods: {
+    addMember: function(){
+      // メンバーのidを配列で取得
+      const ids = this.team.map(function(member){
+        return member.id;
+      });
+      // 配列idsの要素を2つずつ比較して大きい方を残す
+      let maxId = ids.reduce(function(a, b){
+        return Math.max(a, b);
+      });
+      // メンバーの要素を追加
+      this.team.push(
+        { name: '新規メンバー', id: ++maxId }
+      );
+    }
   }
 })
