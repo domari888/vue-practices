3ea4db06155287339f313d4db5d99d99495e3594
damari <motokin128@gmail.com>
2023-01-03 03:47:05 +0900

    add: 親子コンポーネント間でのデータ通信
    子コンポーネント側でデータが変更されたときのイベント発火が確認された場合に親コンポーネント側のメソッドが実行されるように設定

--- communication_between_components/index.js
+++ communication_between_components/index.js
@@ -1,6 +1,13 @@
 const childComponent = {
-  template: '<p>【子コンポーネント】 年齢 : {{age}}歳</p>',
-  props: ['age']
+  template: '<p>【子コンポーネント】 年齢 : {{age}}歳\
+  <button v-on:click="emitAgeEvent">年齢 + 1</button></p>',
+  props: ['age'],
+  methods: {
+    emitAgeEvent: function(){
+      // 第2引数に親コンポーネントへ渡す値を設定
+      this.$emit('change-age', this.age)
+    }
+  }
 };
 
 const parentComponent = {
@@ -14,7 +21,13 @@ const parentComponent = {
   components: {
     'child-component' : childComponent
   },
-  template: '<p>【親コンポーネント】 名前 : {{name}}, 学籍番号 : {{number}}, 年齢 : {{age}}歳<child-component v-bind:age="age"></child-component></p>'
+  template: '<p>【親コンポーネント】 名前 : {{name}}, 学籍番号 : {{number}}, 年齢 : {{age}}歳<child-component v-bind:age="age" v-on:change-age="getAge"></child-component></p>',
+  methods: {
+    // 引数には子コンポーネントから渡された値が入る
+    getAge: function(childAge){
+      this.age = ++childAge;
+    }
+  }
 };
 
 const introduction = new Vue({
