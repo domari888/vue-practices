9629330fc0d27ad4686c38242c5c5452ea7c2a13
damari <motokin128@gmail.com>
2023-01-02 15:09:09 +0900

    add: ローカル登録で複数のカウンターボタンのコンポーネントを作成
    インスタンスを複数作成したとき各インスタンスが独立するように data オプションは関数で定義

--- component/index.js
+++ component/index.js
@@ -1,7 +1,25 @@
+// グローバル登録
 Vue.component('my-name', {
   template: '<p>私の名前は太郎です</p>'
 });
 
 const introduction = new Vue({
   el: '#introduction'
+});
+
+// ローカル登録
+const buttonCounter = {
+  data: function(){
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">カウンター : {{count}}</button>'
+}
+
+const counter = new Vue({
+  el: '#counter',
+  components: {
+    'button-counter' : buttonCounter
+  }
 });
